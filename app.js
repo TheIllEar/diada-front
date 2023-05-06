201a82aef09cd3cdddc73f115b7b1d0174eec02b
TheIllEar <theillear@gmail.com>
2023-05-06 19:00:02 +0300

    remove htaccess

--- app.js
+++ app.js
@@ -6,19 +6,19 @@ import nunjucks from 'nunjucks';
 import nocache from 'nocache';
 import mainRoute from './project/routes/main.js';
 import aboutRoute from './project/routes/about.js';
-// import catalogRoute from './project/routes/works.js';
 import cardRoute from './project/routes/card.js';
-import RewriteMiddleware from 'express-htaccess-middleware';
+// import catalogRoute from './project/routes/works.js';
+// import RewriteMiddleware from 'express-htaccess-middleware';
 
 const app = express(),
   __filename = fileURLToPath(import.meta.url),
   __dirname = dirname(__filename),
-  PORT = process.env.PORT || 3000,
-  RewriteOptions = {
-    file: path.resolve(__dirname, '.htaccess'),
-    verbose: (process.env.ENV_NODE == 'development'),
-    watch: (process.env.ENV_NODE == 'development'),
-  };
+  PORT = process.env.PORT || 3000;
+  // RewriteOptions = {
+  //   file: path.resolve(__dirname, '.htaccess'),
+  //   verbose: (process.env.ENV_NODE == 'development'),
+  //   watch: (process.env.ENV_NODE == 'development'),
+  // };
 
 //Указываем для нунчак расширение файлов, которое будем использовать
 app.set('view engine', 'html');
@@ -34,7 +34,7 @@ app.use(nocache());
 app.set('etag', false);
 
 // .htaccess
-app.use(RewriteMiddleware(RewriteOptions));
+// app.use(RewriteMiddleware(RewriteOptions));
 
 //static and routes
 app.use(express.static(path.join(__dirname, 'project/static')));
